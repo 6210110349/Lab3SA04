561ec8e0973ceca8d26ceae41602e8ef116773a3
6210110349 <6210110349@psu.ac.th>
2021-07-12 11:31:18 +0700

    part4 : connect

--- component/Weather.js
+++ component/Weather.js
@@ -1,21 +1,40 @@
 import React from "react";
 import { Text,StyleSheet,ImageBackground,View } from "react-native";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Forecast from "./Forcast";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
         main: '-',
         description: '-',
         temp: 0
     })
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+            if (props.zipCode) {
+                fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=a8367b27fb8a3aeaac60724d023d590a
+                `)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp});
+                    })
+                .catch((error) => {
+                    console.warn(error);
+                });
+            }
+        }, [props.zipCode])
+    
     return (
         <View>
             <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-                <View style={styles.backdrop2}>
+                
                     <Text >Zip Code</Text>
                     <Text>{props.zipCode}</Text>
                     <Forecast {...forecastInfo} />
-                </View>
+                
             </ImageBackground>
         </View>
     );
@@ -30,11 +49,15 @@ const styles = StyleSheet.create({
         height: '100%'
     },
 
-    backdrop2 : {
-        height : '40%',
-        backgroundColor : 
+   // backdrop2 : {
+    //    height : '40%',
+    //    backgroundColor : '#000',
+    //    fontSize : 10,
+        
+        
 
-    }
+   // },
+    
 });
    
    
