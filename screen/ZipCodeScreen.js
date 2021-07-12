5108ddbe990feccada133507fbf314cda8a9cf21
6210110349 <6210110349@psu.ac.th>
2021-07-12 12:17:54 +0700

    part 5 : update

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,29 +1,33 @@
 import React from 'react'
-import { FlatList } from 'react-native'
+import { FlatList , View , Text} from 'react-native'
+import { useNavigation } from '@react-navigation/core'
+import { StatusBar } from 'expo-status-bar'
 
 const availableZipItems = [
- { place: 'Hatyai', code: '90110' },
- { place: 'Trang', code: '92000' },
- { place: 'Chiangmai', code: '50000' },
- { place: 'Khonkaen', code: '40000' },
- { place: 'Chonburi', code: '20000' },
-]
-
-const ZipItem = ({place,code}) => {
+    { place: 'Hatyai', code: '90110' },
+    { place: 'Trang', code: '92000' },
+    { place: 'Chiangmai', code: '50000' },
+    { place: 'Khonkaen', code: '40000' },
+    { place: 'Chonburi', code: '20000' },
+   ]
+const ZipItem = ({place, code, navigation}) => (
     <View>
-        <Text>{place}</Text>
-        <Text>{code}</Text>
+    <Text>{place}</Text>
+    <Text>{code}</Text>
     </View>
-
-}
-
+    )
+const _keyExtractor = item => item.code
 export default function ZipCodeScreen(){
-    return(
-        <FlatList
-            data = {availableZipItems}
-            keyExtracter = {item => item.code}
-            renderItem = {({item}) => <ZipItem {...item}/>}
-        />
-        )
-
-}
+    const navigation = useNavigation()
+    return (
+    <View>
+    <FlatList
+    data={availableZipItems}
+    keyExtractor={_keyExtractor}
+    renderItem={({item}) => <ZipItem {...item} navigation={navigation}/>}
+    />
+    <StatusBar style="auto" />
+    </View>
+    );
+   
+   }
